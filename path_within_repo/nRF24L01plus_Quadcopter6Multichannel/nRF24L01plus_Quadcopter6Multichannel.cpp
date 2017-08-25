f3a5fec92521ee4d44575cd51d69d4a14b5ca1ce
vaj4088 <ianshef@yahoo.com>
2017-08-25 15:44:51 -0500

    Init Commit per https://eclipsesource.com/blogs/tutorials/egit-tutorial/

---
+++ path_within_repo/nRF24L01plus_Quadcopter6Multichannel/nRF24L01plus_Quadcopter6Multichannel.cpp
@@ -0,0 +1,15 @@
+// Do not remove the include below
+#include "nRF24L01plus_Quadcopter6Multichannel.h"
+
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
---
+++ path_within_repo/nRF24L01plus_Quadcopter6Multichannel/nRF24L01plus_Quadcopter6Multichannel.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _nRF24L01plus_Quadcopter6Multichannel_H_
+#define _nRF24L01plus_Quadcopter6Multichannel_H_
+#include "Arduino.h"
+//add your includes for the project nRF24L01plus_Quadcopter6Multichannel here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project nRF24L01plus_Quadcopter6Multichannel here
+
+
+
+
+//Do not add code below this line
+#endif /* _nRF24L01plus_Quadcopter6Multichannel_H_ */
